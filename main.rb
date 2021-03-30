be80b109d4a0151cdc00769c0c22248ae715548a
Sandra To <sandra.to.e@gmail.com>
2021-03-30 02:19:13 +0000

    Working codes initializing Player class

--- main.rb
+++ main.rb
@@ -6,8 +6,13 @@ require './turn'
 # Test codes below
 
 # game.rb
-game = Game.new
-puts game
-puts game.game_status
-game.game_status = "game_over"
-puts game.game_status
+# game = Game.new
+# puts game
+# puts game.game_status
+# game.game_status = "game_over"
+# puts game.game_status
+
+# player.rb
+player = Player.new("Bob")
+puts player
+puts player.id
--- player.rb
+++ player.rb
@@ -1,1 +1,16 @@
+class Player
+  attr_accessor :current_score, :id, :name
 
+  @@number_of_players = 0
+
+  def initialize(name)
+    @@number_of_players += 1
+    @name = name
+    @id = self.total_number_of_players
+    @current_score = 3
+  end
+
+  def total_number_of_players
+    @@number_of_players
+  end
+end
