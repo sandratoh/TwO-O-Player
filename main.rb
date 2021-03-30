00977de9c2500b5e6bfb4d82e1c5703e4d383a71
Sandra To <sandra.to.e@gmail.com>
2021-03-30 05:42:35 +0000

    Add pseudocode before game setup

--- main.rb
+++ main.rb
@@ -3,25 +3,59 @@ require './player'
 require './question'
 require './turn'
 
+# Game logic here
+
+# Start game
+
+
+# Enter player 1 name
+
+
+
+# Enter player 2 name
+
+
+
+# Turn loop
+
+
+# Question
+
+
+# Player answer
+
+
+# Turn evaluate answer
+
+
+# Check game stat
+
+
+# Repeat or game over
+
+
+
+
+
 # Test codes below
 
 # game.rb
-game = Game.new(gets.chomp, gets.chomp)
-p game
-puts game.game_stat
-puts ''
-game.decrease_player_score(1)
-puts game.game_stat
-# puts game.game_over?    # => false
-puts ''
-game.decrease_player_score(2)
-puts game.game_stat
-game.decrease_player_score(2)
-puts game.game_stat
+# game = Game.new(gets.chomp, gets.chomp)
+# p game
+# puts game.game_stat
+# puts ''
+# game.decrease_player_score(1)
+# puts game.game_stat
+# # puts game.game_over?    # => false
+# puts ''
 # game.decrease_player_score(2)
 # puts game.game_stat
-# puts game.game_over?    # => true
-puts game.winner_stat
+# game.decrease_player_score(2)
+# puts game.game_stat
+# game.decrease_player_score(2)
+# puts game.game_stat
+# # puts game.game_over?    # => true
+# puts game.winner_stat
 # puts game.game_status
 # game.game_status = "game_over"
 # puts game.game_status
