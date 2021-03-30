55bf43ab8a30ed4638382423339d79ae8dfc873d
Sandra To <sandra.to.e@gmail.com>
2021-03-30 07:57:59 +0000

    Move indirectly used methods to protected access

--- game.rb
+++ game.rb
@@ -9,6 +9,27 @@ class Game
     @player2 = Player.new(player2)
   end
 
+  def start
+    puts "Let's get this Math Game started!"
+
+    # Game loop
+    while self.game_status == 'ongoing_game' do
+      turn = Turn.new
+      puts turn.question_to_player
+      
+      # Player answer
+      print '> '
+      turn.player_answer(gets.chomp.to_i)
+      
+      # Evaluate answer and check game stat
+      !turn.answer_correctly? && self.decrease_player_score(turn.player_id)
+      puts turn.turn_response
+      puts self.winner_stat || self.game_stat
+    end
+  end
+
+  protected
+
   def game_stat
     p1_score = @player1.current_score
     p2_score = @player2.current_score
@@ -30,27 +51,6 @@ class Game
       "#{winner.name} wins with a score of #{winner.current_score}/#{MAX_LIVES}!\n----- GAME OVER -----\nGood bye!"
     end
   end
-
-  def start
-    puts "Let's get this Math Game started!"
-
-    # Game loop
-    while self.game_status == 'ongoing_game' do
-      turn = Turn.new
-      puts turn.question_to_player
-      
-      # Player answer
-      print '> '
-      turn.player_answer(gets.chomp.to_i)
-      
-      # Evaluate answer and check game stat
-      !turn.answer_correctly? && self.decrease_player_score(turn.player_id)
-      puts turn.turn_response
-      puts self.winner_stat || self.game_stat
-    end
-  end
-
-  protected
 
   def game_over?
     @player1.is_alive? && @player2.is_alive? ? false : true
